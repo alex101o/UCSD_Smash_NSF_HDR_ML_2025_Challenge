45b51ae0eab80e0cec86b5f80061297ecde708a2
hanx-shi <hanxshi@gmail.com>
2026-01-18 00:11:54 -0800

    Edited notebook

--- color_model_test.py
+++ color_model_test.py
@@ -3,3 +3,4 @@ from ultralytics import YOLO26
 model = YOLO26.from_pretrained("colour-science/colour-checker-detection-models")
 source = 'http://images.cocodataset.org/val2017/000000039769.jpg'
 model.predict(source="colortest1.jpg", save=True)
+
